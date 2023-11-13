a4f08857d9f53508fa3b9f4fe2e723e4d05d47cc
Simon Elbrink <simon.elbrink@lexicon.se>
2023-11-13 12:07:50 +0100

    Variables + Operators + If-statement

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -3,5 +3,104 @@ package se.lexicon;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Group 48!");
+
+
+        /*
+
+        // Declared a Variable
+        // DATATYPE name = VALUE;
+        byte redColorCode = 127;
+        byte greenColorCode = -128;
+        byte blueColorCode = 0;
+
+        short temperature = -300;
+        short groupNumber = 48;
+
+        int salary = 600000;
+
+        long bigNumber = 600_000_000L;
+        long bankBalance = 999999999999999999L;
+
+        float PIE = 3.14f;
+        float GRAVITY = 9.8F;
+
+        boolean studentPresent = true;
+
+        char a = 'a';
+        char TestCharA = 'A';
+
+        char hashtag = '\u0023';
+        char omega = '\u03A9';
+
+        System.out.println(redColorCode);
+        System.out.println(studentPresent);
+        System.out.println(omega);
+
+        int number1;
+        number1 = 10;
+
+        int number2 = 5;
+
+        number1 = number2; // number1 = 5
+        System.out.println(number1);
+
+        int result = 42 + 10;
+        System.out.println(result); //52
+
+        System.out.println(number1 + number2); //10
+
+        System.out.println(10 * 5); // 50
+        System.out.println(10 / 2); // 5
+        
+        
+        int counter = 10;
+
+        counter++; // counter = counter + 1
+
+        System.out.println("counter = " + counter);
+
+        int n1 = 10;
+        int n2 = 5;
+
+        int result1 = n1 / n2;
+
+        int number10 = 10;
+        System.out.println(number10++); // 1.print 10 , 2.increment by 1
+
+
+        int number20 = 20;
+        System.out.println(++number20); //1. increment by 1 , 2.Print 21
+
+
+
+        boolean result10 = number10 < number20;
+        System.out.println("result10 = " + result10);
+
+         */
+
+        /*
+        * 1. If grade is 65 or above = you passed
+        * 2. if grade is 55 or above = your close but not passed
+        * 3. else not passed
+        * */
+
+        int grade = 65;
+
+        if(grade >= 65){
+            System.out.println("You passed the Test!");
+        } else if (grade >= 55){
+            System.out.println("You almost passed the Test!");
+        } else{
+            System.out.println("You did not Pass The Test!");
+        }
+
+
+
+
+
+
+
+
+
     }
 }
