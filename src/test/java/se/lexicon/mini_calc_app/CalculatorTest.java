24aa884dd4d47dd84bd4c82a87dd9b8b0d9cf44a
Simon Elbrink <simon.elbrink@lexicon.se>
2023-11-22 12:32:18 +0100

    Add - Junit testing Framework

---
+++ src/test/java/se/lexicon/mini_calc_app/CalculatorTest.java
@@ -0,0 +1,15 @@
+package se.lexicon.mini_calc_app;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+
+    @Test
+    void First_test() {
+        boolean implemented = false;
+        assertTrue(implemented);
+    }
+}
